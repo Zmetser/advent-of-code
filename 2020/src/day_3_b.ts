4206d4d2132c5d24bbb0516654b989e2ce44e758
Oliver Kovacs <zmetserdev@gmail.com>
2020-12-03 08:53:56 +0100

    Day 3: Toboggan Trajectory - recurse

--- 2020/src/day_3_b.ts
+++ 2020/src/day_3_b.ts
@@ -5,31 +5,26 @@ const input: string[][] =
   readFileSync(path.join(__dirname, '..', 'inputs', 'day3.txt'), { encoding: 'utf-8' })
     .split('\n').map(row => row.split(''));
 
-function check(row: number, col: number) {
-  let i = 0;
-  let j = 0;
-  let trees = 0;
-
-  while (input[i]) {
-    if (input[i][j] === '#') {
-      trees++;
+type Step = [right: number, down: number];
+function slide(slope: string[][], step: Step) {
+  const iter = (i = 0, j = 0, trees = 0): number => {
+    if (i >= slope.length) {
+      return trees;
     }
-    j += col;
-    if (j >= input[i].length) {
-      j = j - input[i].length;
-    }
-    i += row;
+    const nextI = j + step[0];
+    return iter(
+      i + step[1],
+      // wrap around
+      nextI >= slope[i].length ? nextI - slope[i].length : nextI,
+      slope[i][j] === '#' ? trees + 1 : trees
+    );
   }
-  return trees;
+
+  return iter();
 }
-const startingPoints = [
-  [1, 1],
-  [3, 1],
-  [5, 1],
-  [7, 1],
-  [1, 2],
-];
 
-const result = startingPoints.reduce((acc, p) => acc * check(p[1], p[0]), 1);
+const startingPoints: Step[] = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
 
-console.log(result);
+console.log(
+  startingPoints.reduce((acc, p) => acc * slide(input, p), 1)
+);
