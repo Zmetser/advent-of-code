0756892d409e743f6b9be95d43bea7eaa74b8c2f
Oliver Kovacs <zmetserdev@gmail.com>
2020-12-06 21:01:00 +0100

    Day 6: Custom Customs - a lot more flat

--- 2020/src/day_6.ts
+++ 2020/src/day_6.ts
@@ -3,24 +3,14 @@ import { readFileSync } from 'fs';
 
 const input: string[] =
   readFileSync(path.join(__dirname, '..', 'inputs', 'day6.txt'), { encoding: 'utf-8' })
-    .split('\n');
-
-const last = <T>(arr: T[]): T => {
-  return arr[arr.length - 1];
-}
-const emptySet: Set<string> = new Set();
-
-const answersByGroup = input.reduce((acc: Array<Set<string>>, n: string) => {
-  // start new group
-  if (n === '') {
-    return acc.concat([new Set()]);
-  }
-  // add each char in n to the end of the current group
-  const group = last(acc);
-  n.split('').forEach(n => group.add(n));
-  return acc.slice(0, -1).concat([group]);
-}, [emptySet]);
+    .split(/\n{2}/);
 
 console.log(
-  answersByGroup.reduce((acc, n) => acc + n.size, 0)
+  input.reduce((sum, group) =>
+    sum + new Set(
+      Array.from(
+        group.split('\n').map(n => n.split('')).flat()
+      )
+    ).size
+  , 0)
 );
--- 2020/src/day_6_b.ts
+++ 2020/src/day_6_b.ts
@@ -3,22 +3,13 @@ import { readFileSync } from 'fs';
 
 const input: string[] =
   readFileSync(path.join(__dirname, '..', 'inputs', 'day6.txt'), { encoding: 'utf-8' })
-    .split('\n');
-
-type question = string;
-const answersByGroup = input.reduce((acc: Array<Array<Set<question>>>, n: string) =>
-  // start new group
-  (n === '') ?
-    acc.concat([[]]) :
-    // add each question in the answer to the end of the current group
-    acc.slice(0, -1).concat([acc[acc.length - 1].concat([new Set(n.split(''))])])
-  , [[]]);
+    .split(/\n{2,}/g);
 
 const intersect = <T>(a: Set<T>, b: Set<T>): Set<T> => new Set(Array.from(a).filter(x => b.has(x)));
 
 console.log(
-  answersByGroup.reduce((acc, group) =>
-    // Interscet all answers in the group. What remains are the all yes answers
-    acc + group.reduce((acc, _, i, group) => group[i + 1] ? intersect(acc, group[i + 1]) : acc, group[0]).size
-    , 0)
+  input.reduce((result, group) => {
+    const answers = group.split('\n').map(answer => new Set(Array.from(answer.split(''))));
+    return result + answers.reduce((count, _, i, answers) => answers[i + 1] ? intersect(count, answers[i + 1]) : count, answers[0]).size
+  }, 0)
 );
