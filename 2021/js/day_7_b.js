59629165b62af38525e3fab245382f8c1d0fef4e
Oliver Kovacs <zmetser@gmail.com>
2021-12-07 09:32:34 +0100

    Day 7: The Treachery of Whales

---
+++ 2021/js/day_7_b.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+
+const positions = fs.readFileSync(path.join(__dirname, '..', 'inputs', 'day6.txt'), { encoding: 'utf-8' })
+  .split(',').map(Number);
+
+const min = Math.min(...positions);
+const max = Math.max(...positions);
+
+let m = Infinity;
+for (let i = min; i <= max; i++) {
+  const fuelCost = sumOfFuelCostAt(i, positions);
+  if (m > fuelCost) {
+    m = fuelCost;
+  }
+}
+
+console.log(m)
+
+function sumOfFuelCostAt(position, positions) {
+  return positions.reduce((acc, curr) => {
+    const steps = Math.abs(position - curr);
+    return acc + (steps * (1 + steps)) / 2;
+  }, 0);
+}
